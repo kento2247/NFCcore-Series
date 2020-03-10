91d8d9f55835ad21f3c0723c80894024d6ce9682
KENTO <42343541+kento2247@users.noreply.github.com>
2020-03-10 20:24:26 +0900

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,48 @@
+void setup() {
+  nfc_setup_uno();
+  delay(1000);
+}
+
+
+#define Pze_speaker 2
+void loop() {
+  while (!nfc_check());
+  tone(Pze_speaker, 1700);
+  delay(30);
+  noTone(Pze_speaker);
+  delay(30);
+  tone(Pze_speaker, 1700, 70);
+
+
+
+
+
+//  byte setKey[6] = {5,5,5,5,5,5};
+//  nfc_key_change(1,setKey);
+  
+//  nfc_antenna_state(false);
+//  for(byte i; i<20; i++) {
+//    if(nfc_check()) break;
+//    Serial.println(i);
+//    delay(100);
+//  }
+//  nfc_antenna_state(true);
+//  while (!nfc_check());
+  nfc_uid();
+  nfc_cardtype();
+  
+//  byte NEW_UID[4] = {0x59, 0xa5, 0x60, 0xa3};
+//  byte NEW_UID[4] = {0xa5, 0xa5, 0xa5, 0xa3};
+//  nfc_uid_change(NEW_UID);
+
+//    byte Data[18];
+//    nfc_read(Data, 0);
+//    byte WriteData[16] = {0,0,0,0};
+//    for(byte i = 4; i < 16; i++) {
+//      WriteData[i] = Data[i];
+//    }
+//    nfc_write(WriteData, 0);
+  //  nfc_read_name();
+  //  nfc_write_name("kento","tokura");
+  delay(1000);
+}
